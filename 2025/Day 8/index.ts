5003a3964fe3aeba98122477b9fefc705c233377
Adam Iverson <88250950+AdamIverson@users.noreply.github.com>
2025-12-12 21:07:02 -0600

    Day 8 part 1 part 1: lowest distance found

--- 2025/Day 8/index.ts
+++ 2025/Day 8/index.ts
@@ -1,84 +1,71 @@
 import input from './input.txt';
 
 const rawData = input.split('\n').map(item => item.split(',').map(Number));
-
-// console.log("rawData", rawData);
-
+const numberData = rawData.map(item => item.map(Number));
+// console.log("numberData", numberData);
 const n = rawData.length;
-let dataCopy = rawData
-console.log("dataCopy", dataCopy);
 
-const pairComparator = (pointA: number[], pointB: number[], index: number, jindex: number) => {
-  const distance = Math.sqrt(Math.pow(pointA[0]! - pointB[0]!, 2) + Math.pow(pointA[1]! - pointB[1]!, 2) + Math.pow(pointA[2]! - pointB[2]!, 2));
-  return { distance, index, jindex };
+const distanceProcessor = (pointA: number[], pointB: number[], indexA: number, indexB: number) => {
+  // console.log("pointA", pointA);
+  // console.log("pointB", pointB);
+  const distance = Math.sqrt(Math.pow(pointA[0] - pointB[0], 2) + Math.pow(pointA[1] - pointB[1], 2) + Math.pow(pointA[2] - pointB[2], 2));
+  // console.log("distance, indexA, indexB", distance, indexA, indexB);
+  return { distance, indexA, indexB };
 }
 
-let distances = [];
 
-const distanceProcessor = () => {
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = i + 1; j < n; j++) {
-      distances.push(pairComparator(rawData[i], rawData[j], i, j));
+const shortestDistance = (distances: any[]) => {
+  const shortest = distances.sort((a, b) => a.distance - b.distance)[0];
+  return shortest;
+}
+
+const rowDistances = (row: number[], index: number) => {
+  let distances = [];
+  // console.log("row", row);
+  // console.log("index", index);
+  for (let i = index + 1; i < n; i++) {
+    // console.log("i", i);
+    const result = distanceProcessor(numberData[index], numberData[i], index, i);
+    // console.log("result", result);
+    distances.push(result);
+  }
+  // console.log("distances", distances);
+  // Guard against empty distances array
+  if (distances.length === 0) {
+    return null;
+  }
+  const shortie = shortestDistance(distances);
+  return shortie;
+}
+
+let circuits: number[][] = [];
+
+let winner: number | null = null;
+
+const winnerFinder = (rowMin: number) => {
+  // console.log("rowMin", rowMin);
+  if (rowMin) {
+    // console.log("winner", winner);
+    if (winner === null || rowMin < winner) {
+      console.log("rowMin less than winner", rowMin, winner);
+      winner = rowMin;
+    } else if (winner !== null && rowMin > winner) {
+      console.log("rowMin greater than winner", rowMin, winner);
     }
   }
-  return distances;
-}
+};
 
-distanceProcessor();
-console.log("distances", distances);
-const lowestNumber = (array: any[]) => {
-  if (dataCopy.length === 0) return;
-  let min = array[0]?.distance;
-  let intersection = { index: 0, jindex: 0 };
-  for (let i = 1; i < dataCopy.length; i++) {
-    if (distances[i].distance < min) {
-      min = distances[i].distance;
-      intersection = { index: distances[i].index, jindex: distances[i].jindex };
-    }
+const batchProcessor = (data: number[][]) => {
+  // console.log("data", data);
+  let rowMin = null;
+  for (let i = 0; i < data.length; i++) {
+    console.log("i", i);
+    const rowMin = rowDistances(data[i], i);
+    // console.log("rowMin", rowMin);
+    winnerFinder(rowMin?.distance);
   }
-  console.log("intersection", intersection);
-  console.log("min", min);
-  return { min, intersection };
 }
 
-// console.log("lowestNumber", lowestNumber(distances));
 
-const circuits = [];
-const circuitProcessor = ({ index, jindex }) => {
-  console.log("processing pair", [index, jindex]);
-  for (let i = 0; i < circuits.length; i++) {
-    if (!circuits[i].includes(index) && !circuits[i].includes(jindex)) {
-      console.log("new pair", [index, jindex]);
-      circuits.push([index, jindex]);
-    } else if (circuits[i].includes(index) && !circuits[i].includes(jindex)) {
-      console.log("index match")
-      circuits[i].push(jindex);
-    } else if (!circuits[i].includes(index) && circuits[i].includes(jindex)) {
-      console.log("jindex match")
-      circuits[i].push(index);
-    } else if (circuits[i].includes(index) && circuits[i].includes(jindex)) {
-      console.log("both match")
-      continue;
-    }
-  }
-  return circuits;
-}
-// console.log("circuitProcessor", circuitProcessor({ index, jindex }));
-// console.log("circuits", circuits);
-const findCircuits = () => {
-  const removeLow = () => {
-    for (let i = 0; i < 3; i++) {
-      const lowest = lowestNumber(distances);
-      console.log("lowest.intersection", lowest.intersection);
-      // console.log("circuitProcessor", circuitProcessor(lowest.intersection));
-      let filterA = dataCopy.filter(item => lowest.intersection.index === lowest.intersection.index);
-      let filterB = dataCopy.filter(item => lowest.intersection.jindex === lowest.intersection.jindex);
-      // console.log("dataCopy", dataCopy);
-    }
-  }
-  removeLow();
-  return circuits;
-}
-
-// console.log("findCircuits", findCircuits());
-// console.log("dataCopy", dataCopy);
+console.log("batchProcessor", batchProcessor(numberData))
+console.log("winner", winner);
